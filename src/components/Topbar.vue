6471c687a6b0ae1a8fd3665e488d1c8ca2116a3f
Harut <har.amirkhanyan@gmail.com>
2023-01-15 20:17:14 +0400

    Addgetters in vuex

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -43,7 +43,7 @@
             </router-link>
           </li>
         </template>
-        <template v-if="!isLoggedIn">
+        <template v-if="isAnonymous">
           <li class="nav-item">
             <router-link
               class="nav-link"
@@ -70,14 +70,29 @@
 </template>
 
 <script>
+import {getterTypes, mapGetters} from '@/store/modules/auth'
 import {mapState} from 'vuex'
 
 export default {
   name: 'HaTopbar',
   computed: {
-    ...mapState({
-      currentUser: state => state.auth.currentUser,
-      isLoggedIn: state => state.auth.isLoggedIn
+    // ...mapState({
+    // currentUser: state => state.auth.currentUser,
+    // isLoggedIn: state => state.auth.isLoggedIn
+    // }),
+    ...mapGetters({
+      // currentUser() {
+      //   return this.$store.getters[getterTypes.currentUser]
+      // },
+      // isLoggedIn() {
+      //   return this.$store.getters[getterTypes.isLoggedIn]
+      // },
+      // isAnonymous() {
+      //   return this.$store.getters[getterTypes.isAnonymous]
+      // }
+      currentUser: getterTypes.currentUser,
+      isLoggedIn: getterTypes.isLoggedIn,
+      isAnonymous: getterTypes.isAnonymous
     })
   }
 }
