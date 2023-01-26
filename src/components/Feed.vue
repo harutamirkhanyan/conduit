ae8efaf6048daabf5e6baa13fb5b23ca92c344a5
Harut <har.amirkhanyan@gmail.com>
2023-01-26 23:25:37 +0400

    Add layout for feed

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,26 +1,71 @@
 <template>
   <div>
-Feed 
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Something bad happened</div>
+    <div v-if="feed">
+      <div
+        class="article-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link
+            :to="{name: 'userProfile', params: {slug: article.author.username}}"
+          >
+            <img :src="article.author.image" alt="" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile', params: {slug: article.author.username}
+              }"
+              class="author"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right"></div>
+          Add to Favorites
+        </div>
+        <router-link
+          :to="{name: 'article', params: {slug: article.slug}}"
+          class="preview-link"
+        >
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          Tag List
+        </router-link>
+        <div>Pagination</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import {actionTypes} from '@/store/modules/feed'
-  export default {
-    name: 'HaFeed',
-    props:{
-      apiUrl: {
+export default {
+  name: 'HaFeed',
+  props: {
+    apiUrl: {
       type: String,
       required: true
-      }
-    },
-    mounted() {
-      console.log('init feed')
-      this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
-    },
+    }
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.feed.isLoading,
+      feed: state => state.feed.data,
+      error: state => state.feed.error
+    })
+  },
+  mounted() {
+    console.log('init feed')
+    this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
