1baebc80604f39c16eb715b3755b09821f8797c2
Harut <har.amirkhanyan@gmail.com>
2023-02-07 22:32:30 +0400

    Fixed problems in pagination

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -41,10 +41,10 @@
         <div></div>
       </div>
       <ha-pagination
-        :total="total"
+        :total="feed.articlesCount"
         :limit="limit"
         :current-page="currentPage"
-        :url="url"
+        :url="baseUrl"
       ></ha-pagination>
     </div>
   </div>
@@ -54,6 +54,9 @@
 import {mapState} from 'vuex'
 import {actionTypes} from '@/store/modules/feed'
 import HaPagination from '@/components/Pagination'
+import {limit} from '@/helpers/variables'
+import {stringify, parseUrl} from 'query-string'
+
 export default {
   name: 'HaFeed',
   props: {
@@ -67,9 +70,8 @@ export default {
   },
   data() {
     return {
-      total: 500,
-      limit: 10,
-      currentPage: 5,
+      // total: 500,
+      limit,
       url: '/'
     }
   },
@@ -78,11 +80,36 @@ export default {
       isLoading: state => state.feed.isLoading,
       feed: state => state.feed.data,
       error: state => state.feed.error
-    })
+    }),
+    currentPage(){
+      return Number(this.$route.query.page || '1')
+    },
+    baseUrl(){
+    return this.$route.path
+    },
+    offset(){
+      return this.currentPage*limit-limit
+    }
+  },
+  watch: {
+    currentPage(){
+      this.fetchFeed()
+    }
   },
   mounted() {
-    console.log('init feed')
-    this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
+    this.fetchFeed()
+  },
+  methods:{
+    fetchFeed(){
+    const parsedUrl=parseUrl(this.apiUrl)
+    const stringifiedParams=stringify({
+    limit,
+    offset: this.offset,
+    ...parsedUrl.query
+    })
+    const apiUrlWithParams=`${parsedUrl.url}?${stringifiedParams}`
+    this.$store.dispatch(actionTypes.getFeed, {apiUrl: apiUrlWithParams})
+    }
   }
 }
 </script>
