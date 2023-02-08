d8d77a7b6ec99d8cfa6d171cca83d4fff3d5c199
Harut <har.amirkhanyan@gmail.com>
2023-02-09 00:18:02 +0400

    Add feed links

--- src/views/GlobalFeed.vue
+++ src/views/GlobalFeed.vue
@@ -4,6 +4,7 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-9">
+          <ha-feed-toggler />
           <ha-feed :api-url='apiUrl'></ha-feed>
         </div>
         <div class="col-md-3">
@@ -18,8 +19,9 @@
 import HaFeed from '@/components/Feed.vue'
 import HaPupularTags from '@/components/PopularTags.vue'
 import HaBanner from '@/components/Banner.vue'
+import HaFeedToggler from '@/components/FeedToggler.vue'
 export default {
-  components: {HaFeed, HaPupularTags, HaBanner},
+  components: {HaFeed, HaPupularTags, HaBanner, HaFeedToggler},
   name: 'HaGlobalFeed',
   data() {
     return {
--- src/views/TagFeed.vue
+++ src/views/TagFeed.vue
@@ -4,6 +4,7 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-9">
+          <ha-feed-toggler :tag-name="tagName" />
           <ha-feed :api-url="apiUrl"></ha-feed>
         </div>
         <div class="col-md-3">
@@ -18,14 +19,17 @@
 import HaFeed from '@/components/Feed.vue'
 import HaPupularTags from '@/components/PopularTags.vue'
 import HaBanner from '@/components/Banner.vue'
+import HaFeedToggler from '@/components/FeedToggler.vue'
 export default {
   name: 'HaTagFeed',
-  components: {HaFeed, HaPupularTags, HaBanner},
+  components: {HaFeed, HaPupularTags, HaBanner, HaFeedToggler},
   computed: {
+    tagName() {
+      return this.$route.params.slug
+    },
     apiUrl() {
-      const tagName = this.$route.params.slug
-      console.log(tagName, 'tagname')
-      return `/articles?tag=${tagName}`
+      console.log(this.tagName, 'tagname')
+      return `/articles?tag=${this.tagName}`
     }
   }
 }
