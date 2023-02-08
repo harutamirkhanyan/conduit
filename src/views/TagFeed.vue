97a45f92e02c6bce5abbc5c989e52e26be2fd7e0
Harut <har.amirkhanyan@gmail.com>
2023-02-08 22:29:21 +0400

    Add tagFeed & yourFeed

---
+++ src/views/TagFeed.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home-page">
+    <ha-banner />
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <ha-feed :api-url="apiUrl"></ha-feed>
+        </div>
+        <div class="col-md-3">
+          <ha-pupular-tags></ha-pupular-tags>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HaFeed from '@/components/Feed.vue'
+import HaPupularTags from '@/components/PopularTags.vue'
+import HaBanner from '@/components/Banner.vue'
+export default {
+  name: 'HaTagFeed',
+  components: {HaFeed, HaPupularTags, HaBanner},
+  computed: {
+    apiUrl() {
+      const tagName = this.$route.params.slug
+      console.log(tagName, 'tagname')
+      return `/articles?tag=${tagName}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/YourFeed.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home-page">
+    <ha-banner/>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <ha-feed :api-url='apiUrl'></ha-feed>
+        </div>
+        <div class="col-md-3">
+          <ha-pupular-tags></ha-pupular-tags>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HaFeed from '@/components/Feed.vue'
+import HaPupularTags from '@/components/PopularTags.vue'
+import HaBanner from '@/components/Banner.vue'
+export default {
+  components: {HaFeed, HaPupularTags, HaBanner},
+  name: 'HaYourFeed',
+  data() {
+    return {
+      apiUrl: '/articles/feed'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
