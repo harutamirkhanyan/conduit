be0ec627db700dd7a738f780d1004b8103fac30b
Harut <har.amirkhanyan@gmail.com>
2023-02-09 19:08:25 +0400

    Add layout for ArticleForm

---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Create Article
+    <ha-article-form
+      :initial-values="initialValues"
+      :errors="validationErrors"
+      :is-submitting="isSubmitting"
+      @articleSubmit="onSubmit"
+    ></ha-article-form>
+  </div>
+</template>
+
+<script>
+import HaArticleForm from '@/components/ArticleForm'
+export default {
+  name: 'HaCreateArticle',
+  data() {
+    return {
+      initialValues: {
+        title: '',
+        description: '',
+        body: '',
+        tagList: []
+      },
+      validationErrors: null,
+      isSubmitting: false
+    }
+  },
+  components:{HaArticleForm},
+  methods: {
+    onSubmit(data) {
+      console.log('Onsubmit in createArticle', data)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
