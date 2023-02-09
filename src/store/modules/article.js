01a122e5801870306bd6710339d8ff9fab97ac2f
Harut <har.amirkhanyan@gmail.com>
2023-02-09 14:39:45 +0400

    Add btn for delete article

--- src/store/modules/article.js
+++ src/store/modules/article.js
@@ -9,11 +9,17 @@ const state = {
 export const mutationTypes = {
   getArticleStart: '[article] Get article start',
   getArticleSuccess: '[article] Get article success',
-  getArticleFailure: '[article] Get article failure'
+  getArticleFailure: '[article] Get article failure',
+  
+  deleteArticleStart: '[article] Delete article start',
+  deleteArticleSuccess: '[article] Delete article success',
+  deleteArticleFailure: '[article] Delete article failure',
+
 }
 
 export const actionTypes = {
-  getArticle: '[article] Get article'
+  getArticle: '[article] Get article',
+  deleteArticle: '[article] Delete article'
 }
 
 const mutations = {
@@ -27,7 +33,10 @@ const mutations = {
   },
   [mutationTypes.getArticleFailure](state) {
     state.isLoading = false
-  }
+  },
+  [mutationTypes.deleteArticleStart](){},
+  [mutationTypes.deleteArticleSuccess](){},
+  [mutationTypes.deleteArticleSFailure](){}
 }
 
 const actions = {
@@ -43,6 +52,21 @@ const actions = {
         })
         .catch(() => {
           context.commit(mutationTypes.getArticleFailure)
+        })
+    })
+  },
+  
+  [actionTypes.deleteArticle](context, { slug }) {
+    return new Promise(resolve => {
+      context.commit(mutationTypes.deleteArticleStart, slug)
+      articleApi
+        .deleteArticle(slug)
+        .then(() => {
+          context.commit(mutationTypes.deleteArticleSuccess)
+          resolve()
+        })
+        .catch(() => {
+          context.commit(mutationTypes.deleteArticleFailure)
         })
     })
   }
--- src/views/Article.vue
+++ src/views/Article.vue
@@ -24,7 +24,7 @@
               </router-link>
               <span class="date">{{ article.createdAt }}</span>
             </div>
-            <span>
+            <span v-if="isAuthor">
               <router-link
                 class="btn btn-outline-secondary btn-sm"
                 :to="{name: 'editArticle', params: {slug: article.slug}}"
@@ -32,7 +32,10 @@
                 <i class="ion-edit"></i>
                 Edit Article
               </router-link>
-              <button class="btn btn-outline-danger btn-sm">
+              <button
+                class="btn btn-outline-danger btn-sm"
+                @click="deleteArticle"
+              >
                 <i class="ion-trash-a"></i>
                 Delete Article
               </button>
@@ -42,12 +45,12 @@
       </div>
     </div>
     <div class="container page">
-      <ha-loading v-if="IsLoading"></ha-loading>
+      <ha-loading v-if="isLoading"></ha-loading>
       <ha-error-message v-if="error"></ha-error-message>
       <div class="row article-content" v-if="article">
         <div class="xol-xs-12">
           <div>
-            <p>{{article.body}}</p>
+            <p>{{ article.body }}</p>
           </div>
           TAGLIST
         </div>
@@ -57,8 +60,9 @@
 </template>
 
 <script>
-import {actionTypes} from '@/store/modules/article'
-import {mapState} from 'vuex'
+import {actionTypes as articleActionTypes} from '@/store/modules/article'
+import {getterTypes as authGetterTypes} from '@/store/modules/auth'
+import {mapState, mapGetters} from 'vuex'
 import HaLoading from '@/components/Loading.vue'
 import HaErrorMessage from '@/components/ErrorMessage.vue'
 export default {
@@ -68,16 +72,36 @@ export default {
       isLoading: state => state.article.isLoading,
       error: state => state.article.error,
       article: state => state.article.data
-    })
+    }),
+    ...mapGetters({
+      currentUser: authGetterTypes.currentUser
+    }),
+    isAuthor() {
+      if (!this.currentUser || !this.article) {
+        return false
+      }
+      return this.currentUser.username === this.article.author.username
+    }
   },
   components: {
     HaLoading,
     HaErrorMessage
   },
   mounted() {
-    this.$store.dispatch(actionTypes.getArticle, {
+    this.$store.dispatch(articleActionTypes.getArticle, {
       slug: this.$route.params.slug
     })
+  },
+  methods: {
+    deleteArticle() {
+      this.$store
+        .dispatch(articleActionTypes.deleteArticle, {
+          slug: this.$route.params.slug
+        })
+        .then(() => {
+          this.$router.push({name: 'globalFeed'})
+        })
+    }
   }
 }
 </script>
